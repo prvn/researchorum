5a54bbd72d9f787f29286d64de95fdab5ab31f14
Praveen Bathala <pbathala@ebay.com>
2013-11-12 23:43:01 -0800

    [Incomplete] Adding IEEE abstracts fetch support

--- getAcmAbstracts.py
+++ getAcmAbstracts.py
@@ -28,6 +28,8 @@ workQueue = Queue.Queue()
 threads = []
 threadID = 1
 
+kind = ""
+
 class myThread (threading.Thread):
     def __init__(self, threadID, name, q):
         threading.Thread.__init__(self)
@@ -45,7 +47,11 @@ def process_data(threadName, q):
         queueLock.acquire()
         if not workQueue.empty():
             link = q.get()
-            extract_acm_abstract(link)
+            if kind == "acm":
+                extract_acm_abstract(link)
+            elif kind == "ieee":
+                extract_ieee_abstract(link)
+
             queueLock.release()
             #print "%s processing %s" % (threadName, link)
         else:
@@ -56,6 +62,7 @@ def process_data(threadName, q):
 def get_links():
     all_links = []
     acm_links = []
+    ieee_links = []
 
     global client
     global db
@@ -72,6 +79,15 @@ def get_links():
     for i in all_links:
         allowed = True
 
+        # Check if links are IEEE, else pass
+        try:
+            if i.lower().strip().index("ieeecomputersociety") >= 0:
+                ieee_links.append(i.strip())
+        except:
+            pass
+
+        # Check if links are ACM, the do some processing to make 
+        # URL parsable via bot
         try:
             if i.lower().strip().index("acm.org") < 0 and i.lower().strip().index("doi") < 0:
                 allowed = False
@@ -96,10 +112,11 @@ def get_links():
             acm_links.append(str(i) + str("&preflayout=flat"))
 
     print "Total ACM links: ", len(acm_links)
-    print acm_links[:10]
-    return acm_links
+    print "Total IEEE links: ", len(ieee_links)
+    print ieee_links[:10]
+    return (acm_links, ieee_links)
 
-def get_abstracts(links):
+def get_abstracts(links, kind):
     global workQueue
     global queueLock
     global threadList
@@ -127,6 +144,26 @@ def get_abstracts(links):
     for t in threads:
         t.join()
 
+def extract_ieee_abstract(link):
+    resp = None
+    data = None
+    status = 0
+    global collection
+
+    if USE_REQUESOCKS:
+        session = requesocks.session() 
+        session.proxies = {'https': 'socks5://127.0.0.1:9050', 'http': 'socks5://127.0.0.1:9050'}
+        resp = session.get(link)
+    else:
+        resp = requests.get(link)
+
+    status = resp.status_code
+    if status == 200:
+        data = resp.text
+
+        soup = bs4.BeautifulSoup(data)
+
+
 def extract_acm_abstract(link):
     resp = None
     data = None
@@ -141,7 +178,7 @@ def extract_acm_abstract(link):
         resp = requests.get(link)
 
     status = resp.status_code
-    if resp.status_code == 200:
+    if status == 200:
         data = resp.text
         try:
             if data.index("ACM Digital Library") >= 0:
@@ -178,6 +215,16 @@ def extract_acm_abstract(link):
 
     print link, " - ", status
 
+def setKind(k):
+    global kind
+    kind = k
+
 if __name__ == "__main__":
-    acm_links = get_links()
-    get_abstracts(acm_links)
+    acm_links, ieee_links = get_links()
+    sys.exit()
+    if sys.argv[1] == "acm":
+        get_abstracts(acm_links, "acm")
+        setKind("acm")
+    elif sys.argv[1] == "ieee":
+        get_abstracts(ieee_links, "ieee")
+        setKind("ieee")
